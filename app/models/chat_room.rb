5d7fb595746244802f64d0bacef3fc5d8523fd71
sbfckabira <sbfc.japan@gmail.com>
2020-05-28 14:57:11 +0900

    first commit

---
+++ app/models/chat_room.rb
@@ -0,0 +1,5 @@
+class ChatRoom < ApplicationRecord
+
+  has_many :chat_messages
+  has_many :chat_room_users
+end
---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+
+  has_many :chat_messages
+  has_many :chat_room_users
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  enum sex: { 男: 0, 女: 1 }
+  
+  mount_uploader :img_name, ImgNameUploader
+
+  validates :img_name, presence: true
+  
+end
